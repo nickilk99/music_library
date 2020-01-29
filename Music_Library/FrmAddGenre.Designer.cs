e5d819d8b6cf93c1d2d60a343de75bfe823c41fa
Nick I <nickilkic@gmail.com>
2020-01-29 15:16:33 -0500

    added menu

---
+++ Music_Library/FrmAddGenre.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Music_Library
+{
+    partial class FrmAddGenre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblType = new System.Windows.Forms.Label();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.addAGenreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addAnArtistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addAnAlbumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.libraryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(53, 109);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(34, 13);
+            this.lblType.TabIndex = 0;
+            this.lblType.Text = "Type:";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addAGenreToolStripMenuItem,
+            this.addAnArtistToolStripMenuItem,
+            this.addAnAlbumToolStripMenuItem,
+            this.libraryToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip2.TabIndex = 2;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // addAGenreToolStripMenuItem
+            // 
+            this.addAGenreToolStripMenuItem.Name = "addAGenreToolStripMenuItem";
+            this.addAGenreToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.addAGenreToolStripMenuItem.Text = "Add a Genre";
+            this.addAGenreToolStripMenuItem.Click += new System.EventHandler(this.addAGenreToolStripMenuItem_Click);
+            // 
+            // addAnArtistToolStripMenuItem
+            // 
+            this.addAnArtistToolStripMenuItem.Name = "addAnArtistToolStripMenuItem";
+            this.addAnArtistToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.addAnArtistToolStripMenuItem.Text = "Add an Artist";
+            // 
+            // addAnAlbumToolStripMenuItem
+            // 
+            this.addAnAlbumToolStripMenuItem.Name = "addAnAlbumToolStripMenuItem";
+            this.addAnAlbumToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.addAnAlbumToolStripMenuItem.Text = "Add an Album";
+            // 
+            // libraryToolStripMenuItem
+            // 
+            this.libraryToolStripMenuItem.Name = "libraryToolStripMenuItem";
+            this.libraryToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.libraryToolStripMenuItem.Text = "Library";
+            // 
+            // FrmAddGenre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblType);
+            this.Name = "FrmAddGenre";
+            this.Text = "Add a Genre";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem addAGenreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addAnArtistToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addAnAlbumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem libraryToolStripMenuItem;
+    }
+}
+
---
+++ Music_Library/FrmAddGenre.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music_Library
+{
+    public partial class FrmAddGenre : Form
+    {
+        public FrmAddGenre()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void addAGenreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            using (FrmAddGenre frm = new FrmAddGenre())
+            {
+                frm.ShowDialog();
+            }
+        }
+    }
+}
