61d7b44ad06611a76780505986ad8526ef68ca48
ZHANZHOU TA <57724490+tazhanzhou@users.noreply.github.com>
2020-01-30 19:23:16 -0500

    add foreign key

--- Music_Library/Models/Song.cs
+++ Music_Library/Models/Song.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +12,11 @@ namespace Music_Library
 
         public int SongId { get; set; }
 
-        public string Title { get; set; }
+        public string SongTitle { get; set; }
 
         public int AlbumId { get; set; }
         public virtual Album Album { get; set; }
-
         public int GenreId { get; set; }
         public virtual Genre Genre { get; set; }
-
     }
 }
